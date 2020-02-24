df0133cf29b117eae9a736ca1df2a9c7394ab893
Kartik2D <kartikvinayak3@gmail.com>
2020-02-24 03:23:37 -0800

    content update

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ Vue.component('vid', {
   props: ['path'],
   template: `
   	<div class="vid-container">
- 	<video loop class="vid" autoplay muted playsinline>
+ 	<video loop class="vid" onclick="play()" autoplay muted playsinline>
 		<source :src='path' type="video/mp4">
 	</video>
 	</div>
   `,
 })
+
+
 
 var app = new Vue({
   el: '#app',
