9d72c474b50f577aef923882843a0d0e9fa97b7c
Kartik2D <kartikvinayak3@gmail.com>
2020-02-23 17:10:22 -0800

    added autoplay on ios

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ Vue.component('vid', {
   props: ['path'],
   template: `
   	<div class="vid-container">
- 	<video loop class="vid" autoplay muted>
+ 	<video loop class="vid" autoplay muted playsinline>
 		<source :src='path' type="video/mp4">
 	</video>
 	</div>
