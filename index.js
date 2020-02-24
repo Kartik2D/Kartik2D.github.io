ec8d3d582e4f1f964bee79bbdda7758de4548a85
Kartik2D <kartikvinayak3@gmail.com>
2020-02-23 16:50:14 -0800

    new website design

---
+++ index.js
@@ -0,0 +1,17 @@
+Vue.component('vid', {
+  props: ['path'],
+  template: `
+  	<div class="vid-container">
+ 	<video loop class="vid" autoplay muted>
+		<source :src='path' type="video/mp4">
+	</video>
+	</div>
+  `,
+})
+
+var app = new Vue({
+  el: '#app',
+  data: {
+//     message: 'Hello Vue!'
+  }
+});
