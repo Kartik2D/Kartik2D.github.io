c1aea0304d9febb3bdff676a96f61f9eda580d96
Kartik2D <kartikvinayak3@gmail.com>
2020-02-24 19:16:08 -0800

    new css + content

--- index.js
+++ index.js
@@ -2,12 +2,33 @@ Vue.component('vid', {
   props: ['path'],
   template: `
   	<div class="vid-container">
- 	<video loop class="vid" onclick="play()" autoplay muted playsinline>
+ 	<video loop class="vid" onclick="play()" onmouseover="play()" autoplay muted playsinline>
 		<source :src='path' type="video/mp4">
 	</video>
 	</div>
   `,
 })
+
+Vue.component('vidcontrol', {
+  props: ['path'],
+  template: `
+  	<div class="vid-container">
+ 	<video class="vid" controls playsinline>
+		<source :src='path' type="video/mp4">
+	</video>
+	</div>
+  `,
+})
+
+Vue.component('kvimg', {
+  props: ['path'],
+  template: `
+  	<div class="vid-container">
+  	<img :src='path' class="vid"></img>
+	</div>
+  `,
+})
+
 
 
 
